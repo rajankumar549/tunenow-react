1d4724a8f83a92b0e9a7e530d08f5114e6641323
Rajan Kumar <rajankumar549@gmail.com>
2019-02-24 03:45:42 +0530

    add build

---
+++ build/precache-manifest.f56097daf44d606137327e1740920e0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "14f1a6edfc6b51126077",
+    "url": "/static/js/main.14f1a6ed.chunk.js"
+  },
+  {
+    "revision": "83e0d73cced526a7d295",
+    "url": "/static/js/2.83e0d73c.chunk.js"
+  },
+  {
+    "revision": "14f1a6edfc6b51126077",
+    "url": "/static/css/main.8dfe7f75.chunk.css"
+  },
+  {
+    "revision": "83e0d73cced526a7d295",
+    "url": "/static/css/2.d429c77a.chunk.css"
+  },
+  {
+    "revision": "e1891e05af95bc37d19e02fc1adb596f",
+    "url": "/index.html"
+  }
+];
